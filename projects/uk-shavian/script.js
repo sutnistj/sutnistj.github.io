75c34ec7e9ffe6554a284af668f3c716296e6dca
PT Linden <toplinden@gmail.com>
2019-08-15 21:47:21 +0300

    shavian: now work

--- projects/uk-shavian/script.js
+++ projects/uk-shavian/script.js
@@ -1,63 +1,69 @@
-"use strict";
-
-var hidden = Math.round(Math.random()*200+0.5);
-console.log(hidden);
-
-var tr = {
-    // clear
-    'а' : '𐑨',
-    'б' : '𐑚',
-    'в' : '𐑫',
-    'г' : '𐑣',
-    'ґ' : '𐑜',
-    'д' : '𐑛',
-    'е' : '𐑧',
-    'ж' : '𐑠',
-    'з' : '𐑟',
-    'и' : '𐑦',
-    'і' : '𐑰',
-    'й' : '𐑘',
-    'к' : '𐑒',
-    'л' : '𐑤',
-    'м' : '𐑥',
-    'н' : '𐑯',
-    'о' : '𐑴',
-    'п' : '𐑐',
-    'р' : '𐑮',
-    'с' : '𐑕',
-    'т' : '𐑑',
-    'у' : '𐑵',
-    'ф' : '𐑓',
-    'х' : '𐑙',
-    'ц' : '𐑔',
-    'ч' : '𐑗',
-    'ш' : '𐑖',
-    'ь' : '𐑢',
-    // disounds
-    'щ' : '𐑖𐑗',
-    // vuk
-    'џ' : '𐑡',
-    'ѕ' : '𐑞',
-    'ј' : '𐑘',
-    // symbols
-    ' ' : ' ',
-    ',' : ',',
-    '.' : '.'
-};
-
-
-function transliterate() {
-    var latin = document.getElementById('send').value;
-    var result = '';
-    var i;
-    for (i = 0; i < latin.length; i++) {
-        console.log(latin[i]);
-        if (tr[ latin[i]] ) {
-            result += tr[ latin[i]];
-        }
+var 
+    small = 'бвгґджзклмнпрстфхцчшщ',
+    // big = small.toUpperCase(),
+    apostrophes = '\'ʼ`’',
+    soft = {
+        'я' : 'ьа',
+        'ю' : 'ьу',
+        'є' : 'ье',
+        'ї' : 'ьі',
+    },
+    ltr = {
+        // clear
+        'а' : '𐑨',
+        'б' : '𐑚',
+        'в' : '𐑫',
+        'г' : '𐑣',
+        'ґ' : '𐑜',
+        'д' : '𐑛',
+        'е' : '𐑧',
+        'ж' : '𐑠',
+        'з' : '𐑟',
+        'и' : '𐑦',
+        'і' : '𐑰',
+        'й' : '𐑘',
+        'к' : '𐑒',
+        'л' : '𐑤',
+        'м' : '𐑥',
+        'н' : '𐑯',
+        'о' : '𐑴',
+        'п' : '𐑐',
+        'р' : '𐑮',
+        'с' : '𐑕',
+        'т' : '𐑑',
+        'у' : '𐑵',
+        'ф' : '𐑓',
+        'х' : '𐑙',
+        'ц' : '𐑔',
+        'ч' : '𐑗',
+        'ш' : '𐑖',
+        'ь' : '𐑢',
+        // disounds
+        'щ' : '𐑖𐑗',
+        'я' : '𐑘𐑨',
+        'ю' : '𐑘𐑵',
+        'є' : '𐑘𐑧',
+        'ї' : '𐑘𐑰',
+        // vuk
+        'џ' : '𐑡',
+        'ѕ' : '𐑞',
+        'ј' : '𐑘',
     }
-    document.getElementById('result').innerHTML = result;
-    return false;
-}
+    ;
 
-document.getElementById('send').addEventListener('keyup', transliterate);
+function shavian()
+{
+    var res = document.getElementById('send').value.toLowerCase();
+
+    for (i in soft) {
+        res = res.replace(new RegExp('([' + small /*+ big*/ + '])' + i, 'g'), "$1" + soft[i]);
+    }
+
+    res = res.replace(new RegExp('[' + apostrophes + ']', 'g'), '');
+
+    for (l in ltr)
+    {
+        res = res.replace(new RegExp(l, 'g'), ltr[l]);
+    }
+    document.getElementById('result').innerHTML = res.replace(/\n\r?/g, '<br />');
+}
