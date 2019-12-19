cb32ca1e4ad1354dfe5ab01081d8fe55668c41bf
PT Linden <toplinden@gmail.com>
2019-12-19 10:49:40 +0200

    uk-latin

---
+++ projects/uk-latin/script.js
@@ -0,0 +1,114 @@
+/** keys */
+
+var small_c = 'бвгґджзклмнпрстфхцчшщџѕ'
+var small_v = 'аяеєіїийьјоую'
+var smalls = small_c + small_v
+
+var big_c = small_c.toUpperCase()
+var bigs = smalls.toUpperCase()
+
+var apostrophe = 'ʼ'
+var apostrophes = '\'ʼ`’'
+
+var u_softs = {
+    'я': 'ьа',
+    'ю': 'ьу',
+    'є': 'ье',
+    'ї': 'ьі',
+}
+
+var u_iots = swap(u_softs)
+
+var u_letters = {
+    // long sounds
+    'чч': 'tcc',
+    'цц': 'tss',
+    // disounds
+    'щ': 'ctc',
+    'ц': 'ts',
+    'ч': 'tc',
+    'я': 'ja',
+    'ю': 'ju',
+    'є': 'je',
+    'ї': 'ji',
+    // vuk
+    'џ': 'dg',
+    'ѕ': 'dz',
+    'ј': 'j',
+    // clear
+    'а': 'a',
+    'б': 'b',
+    'в': 'v',
+    'г': 'h',
+    'ґ': 'q',
+    'д': 'd',
+    'е': 'e',
+    'ж': 'g',
+    'з': 'z',
+    'и': 'y',
+    'і': 'i',
+    'й': 'j',
+    'к': 'k',
+    'л': 'l',
+    'м': 'm',
+    'н': 'n',
+    'о': 'o',
+    'п': 'p',
+    'р': 'r',
+    'с': 's',
+    'т': 't',
+    'у': 'u',
+    'ф': 'f',
+    'х': 'x',
+    'ш': 'c',
+    'ь': 'j',
+}
+
+var s_letters = swap(u_letters)
+
+function swap(object) {
+    var re_object = {}
+    for (key in object) {
+        re_object[object[key]] = key
+    }
+    return re_object
+}
+
+function latin() {
+    var result = document.getElementById('send').value
+
+    /** Основна перебуква */
+    for (letter in u_letters) {
+        var trans = u_letters[letter]
+        result = result
+            .replace(new RegExp(letter, 'g'), trans)
+            /** Великі */
+            .replace(new RegExp(letter.toUpperCase(), 'g'), trans[0].toUpperCase() + trans.slice(1))
+    }
+
+    /** Переноси і відʼапострофуванє */
+    document.getElementById('result').innerHTML = result
+        .replace(/\n\r?/g, '<br />')
+        .replace(new RegExp('[' + apostrophes + ']', 'g'), '')
+}
+
+function cyrillic() {
+    var result = document.getElementById('send').value
+
+    /** Основна перебуква */
+    for (letter in s_letters) {
+        var trans = s_letters[letter]
+        result = result
+            .replace(new RegExp(letter, 'g'), trans)
+            /** Великі */
+            .replace(new RegExp(letter[0].toUpperCase() + letter.slice(1), 'g'), trans.toUpperCase())
+    }
+
+
+    /** Переноси, апострофуванье і відјотуванье*/
+    document.getElementById('result').innerHTML = result
+        .replace(new RegExp('([ ' + small_v + '])ь', 'g'), '$1й')
+        // .replace(new RegExp('([пбвфкґхгм])ь', 'g'), '$1ʼй')
+        .replace(new RegExp('([ ' + small_v.toUpperCase() + '])Ь', 'g'), '$1Й')
+        .replace(/\n\r?/g, '<br />')
+}
--- projects/uk-shavian/script.js
+++ projects/uk-shavian/script.js
@@ -1,70 +1,61 @@
 // Обмін кльучів на змінну в објектах
-function swap(object)
-{
-    var re_object = {};
-    for(key in object){
-        re_object[object[key]] = key;
+function swap(object) {
+    var re_object = {}
+    for (key in object) {
+        re_object[object[key]] = key
     }
-    return re_object;
+    return re_object
 }
 
 // З кирилицьи на шовицьу
-function shavian()
-{
-    var result = document.getElementById('send').value;
+function shavian() {
+    var result = document.getElementById('send').value
 
     // Розкласти јотовані на мјакиј знак і голосну
-    for (soft in u_softs)
-    {
-        result = result.replace(new RegExp('([' + small_c + big_c + '])' + soft, 'g'), "$1" + u_softs[soft]);
+    for (soft in u_softs) {
+        result = result.replace(new RegExp('([' + small_c + big_c + '])' + soft, 'g'), "$1" + u_softs[soft])
     }
-    
+
     result = result
-        // Прибирати апострофи
+        // Відʼапострофнути
         .replace(new RegExp('[' + apostrophes + ']', 'g'), '')
-        // Позначити великі букви
+        // Великі букви
         .replace(new RegExp('([' + bigs + '])', 'g'), '·' + '$1')
         .toLowerCase()
-        ;
 
     // Основна перебуква
-    for (letter in u_letters)
-    {
-        result = result.replace(new RegExp(letter, 'g'), u_letters[letter]);
+    for (letter in u_letters) {
+        result = result.replace(new RegExp(letter, 'g'), u_letters[letter])
     }
 
     // Рьадки
-    document.getElementById('result').innerHTML = result.replace(/\n\r?/g, '<br />');
+    document.getElementById('result').innerHTML = result.replace(/\n\r?/g, '<br />')
 }
 
 // З шовицьи на кирилицьу
-function ukrainian()
-{
-    var result = document.getElementById('send').value;
+function ukrainian() {
+    var result = document.getElementById('send').value
 
     // Основна заміна
-    for (letter in s_letters)
-    {
-        result = result.replace(new RegExp(letter, 'g'), s_letters[letter]);
+    for (letter in s_letters) {
+        result = result.replace(new RegExp(letter, 'g'), s_letters[letter])
     }
 
     // Апострофнути
-    for (iot in u_softs)
-    {
-        result = result.replace(new RegExp('([' + small_c + '])' + iot, 'g'), "$1" + apostrophe + iot);
+    for (iot in u_softs) {
+        result = result.replace(new RegExp('([' + small_c + '])' + iot, 'g'), "$1" + apostrophe + iot)
     }
 
     // Јотација
-    for (soft in u_iotation) 
-    {
-        result = result.replace(new RegExp(soft, 'g'), u_iotation[soft]);
+    for (soft in u_iots) {
+        result = result.replace(new RegExp(soft, 'g'), u_iots[soft])
     }
 
     document.getElementById('result').innerHTML = result
         // Великі букви
-        .replace(new RegExp('·([' + smalls + '])', 'g'), function(match) { return match[1].toUpperCase() })
+        .replace(new RegExp('·([' + smalls + '])', 'g'), function (match) { return match[1].toUpperCase() })
         // Рьадки
-        .replace(/\n\r?/g, '<br />');
+        .replace(/\n\r?/g, '<br />')
 }
 
 var
@@ -79,55 +70,54 @@ var
     apostrophes = '\'ʼ`’',
 
     u_softs = {
-        'я' : 'ьа',
-        'ю' : 'ьу',
-        'є' : 'ье',
-        'ї' : 'ьі',
+        'я': 'ьа',
+        'ю': 'ьу',
+        'є': 'ье',
+        'ї': 'ьі',
     },
-    u_iotation = swap(u_softs),
-    
+    u_iots = swap(u_softs),
+
     u_letters = {
         // disounds
-        'щ' : '𐑖𐑗',
-        'я' : '𐑘𐑨',
-        'ю' : '𐑘𐑵',
-        'є' : '𐑘𐑧',
-        'ї' : '𐑘𐑰',
+        'щ': '𐑖𐑗',
+        'я': '𐑘𐑨',
+        'ю': '𐑘𐑵',
+        'є': '𐑘𐑧',
+        'ї': '𐑘𐑰',
         // vuk
-        'џ' : '𐑡',
-        'ѕ' : '𐑞',
-        'ј' : '𐑘',
+        'џ': '𐑡',
+        'ѕ': '𐑞',
+        'ј': '𐑘',
         // clear
-        'а' : '𐑨',
-        'б' : '𐑚',
-        'в' : '𐑫',
-        'г' : '𐑣',
-        'ґ' : '𐑜',
-        'д' : '𐑛',
-        'е' : '𐑧',
-        'ж' : '𐑠',
-        'з' : '𐑟',
-        'и' : '𐑦',
-        'і' : '𐑰',
-        'й' : '𐑘',
-        'к' : '𐑒',
-        'л' : '𐑤',
-        'м' : '𐑥',
-        'н' : '𐑯',
-        'о' : '𐑴',
-        'п' : '𐑐',
-        'р' : '𐑮',
-        'с' : '𐑕',
-        'т' : '𐑑',
-        'у' : '𐑵',
-        'ф' : '𐑓',
-        'х' : '𐑙',
-        'ц' : '𐑔',
-        'ч' : '𐑗',
-        'ш' : '𐑖',
-        'ь' : '𐑢',
+        'а': '𐑪',
+        'б': '𐑚',
+        'в': '𐑫',
+        'г': '𐑣',
+        'ґ': '𐑜',
+        'д': '𐑛',
+        'е': '𐑧',
+        'ж': '𐑠',
+        'з': '𐑟',
+        'и': '𐑦',
+        'і': '𐑰',
+        'й': '𐑘',
+        'к': '𐑒',
+        'л': '𐑤',
+        'м': '𐑥',
+        'н': '𐑯',
+        'о': '𐑴',
+        'п': '𐑐',
+        'р': '𐑮',
+        'с': '𐑕',
+        'т': '𐑑',
+        'у': '𐑵',
+        'ф': '𐑓',
+        'х': '𐑙',
+        'ц': '𐑔',
+        'ч': '𐑗',
+        'ш': '𐑖',
+        'ь': '𐑢',
     },
     s_letters = swap(u_letters)
-    ;
 
-    // console.log(s_letters);
+    // console.log(s_letters)
