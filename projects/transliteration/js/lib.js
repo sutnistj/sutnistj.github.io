eac79614061dd9971821548d6437a29f9723abb6
Toplinden <toplinden@gmail.com>
2020-10-21 11:47:54 +0300

    massive: design, unification, fonts, glagolitic, abbr

---
+++ projects/transliteration/js/lib.js
@@ -0,0 +1,96 @@
+function valueOfSelection(id) { // string
+    let selObj = document.getElementById(id)
+    return selObj.options[selObj.selectedIndex].text
+} // string
+
+function printResult(string) { // string
+    document.getElementById('result').innerHTML = string.replace(/\n\r?/g, '<br />')
+}
+
+function transliteration(from, to, value) { // string, string, string
+
+    var upCaseLetters = upCase(from, getLetters(from)) + upCase(to, getLetters(to))
+    var softLetters = getLetters(to, 'Soft');
+    var iotaSign = getLetters(to, 'Jot')
+    var softSign = getLetters(to, 'Soft sign')
+
+    console.log('test');
+
+    for (letter of scripts) {
+        if (isUnderfined([letter[from], letter[to]])) { continue }
+
+        // Party uppercase
+        let partlyIn = upCase(from, letter[from], false)
+        let partlyOut = upCase(to, letter[to], false)
+
+        // Full uppercase
+        let exception = '([' + '0-9\-' + upCaseLetters + ']{0,})'
+        let fullIn = '(?<=[\\s,.:;"\']|^)'
+            + exception + upCase(from) + exception
+            + '(?=[\\s,.:;"\']|$)'
+
+        // Iotation
+        let lowerSoft = '([' + softLetters + '])' + iotaSign
+        let upperSoft = '([' + upCase(softLetters) + '])' + upCase(iotaSign)
+
+        value = value
+            .normalize('NFD')
+            .replace(new RegExp(fullIn, 'g'), '$1' + upCase(to) + '$2')
+            .replace(new RegExp(partlyIn, 'g'), partlyOut)
+            .replace(new RegExp(letter[from], 'g'), letter[to])
+            .replace(new RegExp(lowerSoft, 'g'), '$1' + softSign)
+            .replace(new RegExp(upperSoft, 'g'), '$1' + upCase(softSign))
+    }
+    return value
+} // string
+
+function upCase(script, symbol = letter[script], all = true) { // string, string, boolean
+    if (isUnderfined(symbol)) return
+    switch (script) {
+        case 'Shavian': {
+            symbol = all ? '·' + [...symbol].join('·') : '·' + symbol
+            break
+        }
+        default: {
+            symbol = all ? symbol.toUpperCase() : symbol[0].toUpperCase() + symbol.slice(1)
+            break
+        }
+    }
+    return symbol
+} // string
+
+function isUnderfined(value) { // any
+    switch (typeof value) {
+        case 'object': {
+            if (value === undefined) { return true }
+            for (i of value) if (i === undefined) { return true }
+            return false
+        }
+        default: {
+            return value === undefined ? true : false
+        }
+    }
+} // boolean
+
+function getLetters(script, param = 'all') { // array, string
+    var string = ''
+    switch (param) {
+        case 'all': {
+            for (letter of scripts) {
+                if (isUnderfined(letter[script])) { continue }
+                string += letter[script]
+            }
+            break
+        }
+        default: {
+            for (letter of scripts) {
+                if (isUnderfined(letter[script])
+                    || isUnderfined(letter['Param'])
+                    || letter['Param'].indexOf(param) < 0) { continue }
+                string += letter[script]
+            }
+            break
+        }
+    }
+    return string
+} // string
---
+++ projects/transliteration/js/script.js
@@ -0,0 +1,44 @@
+function eraseTextareaAndResult() {
+    document.getElementById('send').value = ""
+    document.getElementById('result').innerHTML = ""
+}
+
+function copyResultToClipboard() {
+    var range = document.createRange()
+    range.selectNode(document.getElementById('result'))
+    window.getSelection().removeAllRanges()
+    window.getSelection().addRange(range)
+    document.execCommand('copy')
+    window.getSelection().removeAllRanges()
+}
+
+function swapValueOfSelections() {
+    var temp = valueOfSelection('from')
+    document.getElementById('from').value = valueOfSelection('to')
+    document.getElementById('to').value = temp
+}
+
+function sendValueOfTextarea() {
+    var to = valueOfSelection('to')
+    var from = valueOfSelection('from')
+    var value = document.getElementById('send').value
+    printResult(to === from ? value : transliteration(from, to, value))
+}
+
+/* Hotkeys */
+var keys
+
+document.getElementById('send').addEventListener("keydown", function (e) {
+    keys = (keys || [])
+    keys[e.key] = true
+
+    // Ctrl + Enter in textarea: send values of textarea
+    if (keys['Control'] && keys['Enter'] && Object.keys(keys).length == 2) {
+        sendValueOfTextarea()
+    }
+}, false)
+
+document.addEventListener("keyup", function (e) {
+    keys = []
+    stop()
+}, false)
